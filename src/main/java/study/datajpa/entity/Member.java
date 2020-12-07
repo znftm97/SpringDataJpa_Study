d719acb96456fcdb4b45ffa7bf45888ebc274730
jihoon <znftm97@naver.com>
2020-12-07 18:05:37 +0900

    예제 도메인 구현 && JPA 리포지토리 생성

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,42 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // protected 생성자 자동 생성
+@ToString(of = {"id", "userName", "age"}) // 연관관계 필드인 team은 빼고, 무한루프에 빠질 수 있음
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String userName;
 
-    protected Member(){
+    private int age;
 
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String userName) {
         this.userName = userName;
+    }
+
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    // 연관관계 메서드
+   public void changeTeam(Team team){
+        this.team = team;
+        team.getMebers().add(this);
     }
 }
--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -6,6 +6,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,7 +20,24 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
     public Member find(Long id){
         return em.find(Member.class, id);
+    }
+
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Long count(){
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
     }
 }
